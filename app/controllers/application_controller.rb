93b38d798c61d35b97a72fa4296f1bb18d722ada
Sebastián Gamboa <me@sagmor.com>
2010-07-14 10:43:54 -0400

    Facebook comments en artefactos.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,19 +7,23 @@ class ApplicationController < ActionController::Base
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
-  def logged_in?
-    session[:user_id].present?
-  end
-  def current_user
-    @current_user ||= Person.find(session[:user_id])
-  end
+  
+  protected
+    def logged_in?
+      session[:user_id].present?
+    end
+    def current_user
+      @current_user ||= Person.find(session[:user_id])
+    end
 
-  def store_location
-    session[:return_to] = request.request_uri
-  end
+    def store_location
+      session[:return_to] = request.request_uri
+    end
 
-  def redirect_back_or_default(default)
-    redirect_to(session[:return_to] || default)
-    session[:return_to] = nil
-  end
+    def redirect_back_or_default(default)
+      redirect_to(session[:return_to] || default)
+      session[:return_to] = nil
+    end
+    
+  helper_method :logged_in?, :current_user
 end
